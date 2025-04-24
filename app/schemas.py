4911005ad085d669bce6597b02e58179c69d0f55
Nikita <152098327+Nihihitik@users.noreply.github.com>
2025-04-24 14:43:56 +0300

    Создание моделей

---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class CreateProduct(BaseModel):
+    name: str
+    description: str
+    price: int
+    image_url: str
+    stock: int
+    category: int
+
+class CreateCategory(BaseModel):
+    name: str
+    parent_id: Optional[int]
+
+
+
+
